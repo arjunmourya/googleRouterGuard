eb6216804d6579c57d6372f939d843cf2f6cf552
Arjun Mourya <arjunmourya88@gmail.com>
2018-01-10 16:05:51 +0530

    removing client id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AuthGuard } from './auth-guard.service';
 
 let providers = {
   "google": {
-    "clientId": "783541848322-fejsuieen9b8n5c9otcdlo7anllbk32o.apps.googleusercontent.com"
+    "clientId": "<your_clientId_on_google>.apps.googleusercontent.com"
   }
   // ,
   // "linkedin": {
